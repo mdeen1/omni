d9ba43039fa946a51ce5de81a3d19874e4392b33
mdeen1 <34013848+mdeen1@users.noreply.github.com>
2018-06-15 16:35:56 -0700

    Add files via upload

--- resources/Jquery/JS/scripts.js
+++ resources/Jquery/JS/scripts.js
@@ -1,5 +1,11 @@
 $(function() {
     
-    
+    $('.js--section-feautures').waypoints(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky')
+        }
+    });
 
 });
